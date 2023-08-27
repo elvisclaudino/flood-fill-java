18a0e56b42b46a6dc9fdc8ba73a8d5dd685928e5
nsx07 <felipeaugustodluz@hotmail.com>
2023-08-27 10:32:22 -0300

    Criando constructor para usar imagens.

--- src/classes/FloodFiller.java
+++ src/classes/FloodFiller.java
@@ -1,21 +1,46 @@
 package classes;
 
-@SuppressWarnings("ALL")
-public class FloodFiller<T> {
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Path;
 
-    private T[][] currentMatrix;
-    private final T[][] matrixOrigin;
+@SuppressWarnings("ALL")
+public class FloodFiller {
+
+    private int[][] currentMatrix;
+    private int[][] matrixOrigin;
+    private BufferedImage originImage;
+    private BufferedImage resultImage;
+
     private Queue<Coordinate> queue = new Queue<>();
     private Stack<Coordinate> stack = new Stack<>();
-    public FloodFiller(T[][] matrixOrigin) {
+    public FloodFiller(int[][] matrixOrigin) {
         this.matrixOrigin = matrixOrigin;
     }
 
-    private T[][] matrix() {
+    public FloodFiller(String imagePath) throws IOException {
+
+        originImage = ImageIO.read(new File(imagePath));
+        resultImage = new BufferedImage(originImage.getWidth(), originImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        initMatrix();
+
+        File outputFile = new File("resources/result.jpg");
+        ImageIO.write(resultImage, "jpg", outputFile);
+    }
+
+    private void initMatrix() {
+          
+    }
+
+    private int[][] matrix() {
         return  currentMatrix == null ? matrixOrigin : currentMatrix;
     }
 
-    protected void matrix(int x, int y, T value) {
+    protected void matrix(int x, int y, int value) {
         matrix()[x][y] = value;
     }
 
@@ -28,17 +53,17 @@ public class FloodFiller<T> {
         System.out.println("}");
     }
 
-    public void paint(int x, int y, T value) throws Exception {
+    public void paint(int x, int y, int value) throws Exception {
         System.arraycopy(matrixOrigin, 0, currentMatrix, matrixOrigin.length - 1, matrixOrigin.length);
         Coordinate coord = new Coordinate(x, y);
         printImage();
-        T toFind = matrix()[coord.getX()][coord.getY()];
+        int toFind = matrix()[coord.getX()][coord.getY()];
         stack.add(coord);
         fill(toFind, value);
         printImage();
         currentMatrix = null;
     }
-    private void fill(T find, T replace) throws Exception {
+    private void fill(int find, int replace) throws Exception {
         if (stack.isEmpty()) {
             return;
         }
@@ -54,35 +79,36 @@ public class FloodFiller<T> {
         } else throw new Exception("Coordenada inválida!");
     }
 
-    private boolean checkX(Coordinate coordinate, T find) {
+    private boolean checkX(Coordinate coordinate, int find) {
         boolean found = true;
 
-        coordinate = new Coordinate(coordinate.getX() + 1, coordinate.getY());
+        coordinate.setX(coordinate.getX() + 1);
         if (checkBottom(coordinate)) {
             if (matrix()[coordinate.getX()][coordinate.getY()] == find) {
                 stack.add(coordinate);
             } else found = false;
         }
 
-        coordinate = new Coordinate(coordinate.getX() - 2, coordinate.getY());
+        coordinate.setX(coordinate.getX() - 2);
         if (checkTop(coordinate)) {
             if (matrix()[coordinate.getX()][coordinate.getY()] == find) {
                 stack.add(coordinate);
             } else found = false;
         }
+
         return found;
     }
-    private boolean checkY(Coordinate coordinate, T find) {
+    private boolean checkY(Coordinate coordinate, int find) {
         boolean found = true;
 
-        coordinate = new Coordinate(coordinate.getX(), coordinate.getY() - 1);
+        coordinate.setY(coordinate.getY() - 1);
         if (checkLeft(coordinate)) {
             if (matrix()[coordinate.getX()][coordinate.getY()] == find) {
                 stack.add(coordinate);
             } else found = false;
         }
 
-        coordinate = new Coordinate(coordinate.getX(), coordinate.getY() + 2);
+        coordinate.setY(coordinate.getY() + 2);
         if (checkRight(coordinate)) {
             if (matrix()[coordinate.getX()][coordinate.getY()] == find) {
                 stack.add(coordinate);
